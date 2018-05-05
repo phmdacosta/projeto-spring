2d501e5365d4d569729b1b4514ca714e5a305d9e
phmdacosta <ph.mcosta@hotmail.com>
2018-05-05 19:54:08 -0300

    Initial commit

---
+++ SpringTest/src/main/java/br/com/example/SpringTest/App.java
@@ -0,0 +1,13 @@
+package main.java.br.com.example.SpringTest;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
